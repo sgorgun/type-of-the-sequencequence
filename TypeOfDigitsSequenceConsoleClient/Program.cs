4781620f17b6b9f9f41e8f6f9bced572e373987a
@gorgun.sergei <gorgun.sergei@student.ehu.lt>
2022-12-11 16:40:12 +0200

    Blank project with console run

--- TypeOfDigitsSequenceConsoleClient/Program.cs
+++ TypeOfDigitsSequenceConsoleClient/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
-using static System.Console;
-using TypeOfDigitsSequenceLogic;
-using static TypeOfDigitsSequenceLogic.IntegersExtension;
+﻿namespace TypeOfDigitsSequenceConsoleClient
+{
+    using System;
+    using static System.Console;
+    using TypeOfDigitsSequenceLogic;
+    using static TypeOfDigitsSequenceLogic.IntegersExtension;
+
+    class Program
+    {
+        private static void Main()
+        {
+            WriteLine();
+            ReadLine();
+
+        }
+    }
+
+}
